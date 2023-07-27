c9385e28e452b426cfe6ffb4e5c2a4b2b812a606
sergeevev <sergeevevit@gmail.com>
2023-07-28 00:29:45 +0500

    homework1: edit readme.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# Python Basic 2023-05
+## Repo for homeworks
+
+_Sergeev Evgeny_
--- homework_01/main.py
+++ homework_01/main.py
@@ -1,25 +1,41 @@
+from itertools import filterfalse
+import math
+
 """
 Домашнее задание №1
 Функции и структуры данных
 """
 
 
-def power_numbers():
+def power_numbers(*nums):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
-
+    return [i ** 2 for i in nums]
+    
+print('power_numbers(2,7,11): ', power_numbers(2,7,11))
+print('power_numbers(1,5,3,71): ', power_numbers(1,5,3,71))
 
 # filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def is_odd(num):
+    return num % 2
 
-def filter_numbers():
+def is_prime(num):
+    if num <=1:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def filter_numbers(nums, condition):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +46,13 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    if condition == 'odd':
+        return list(filter(is_odd, nums))
+    elif condition == 'even':
+        return list(filterfalse(is_odd, nums))
+    else:
+        return list(filter(is_prime, nums))
+    
+print('filter_numbers([1, 2, 3], ODD): ', filter_numbers([1, 2, 3], ODD))
+print('filter_numbers([2, 3, 4, 5], EVEN): ', filter_numbers([2, 3, 4, 5], EVEN))
+print('filter_numbers([2, 7, 4, 11, 22, 26, 1, 0, 93], PRIME): ', filter_numbers([2, 7, 4, 11, 22, 26, 1, 0, 93], PRIME))
